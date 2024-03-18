f3bc3c61caf325d452a8054704e2abb667a33faa
Vinicius Luiz <077viniciu@gmail.com>
2024-03-18 10:09:43 -0300

    descartando o que será usado [inicialmente]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/Banner.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bem vindo ao react.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
