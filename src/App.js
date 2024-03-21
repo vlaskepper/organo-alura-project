7ff65d3d3c299276356fc821855bdc1ea4718d39
Vinicius Luiz <077viniciu@gmail.com>
2024-03-21 16:13:39 -0300

    'ouvindo' o formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador]);
+  };
+
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario 
+      aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -4,7 +4,7 @@ import './Formulario.css';
 import Botao from '../Botao';
 import { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -24,7 +24,12 @@ const Formulario = () => {
     
     const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log('Form foi submetido =>', nome, cargo, imagem, time);
+        props.aoColaboradorCadastrado ({
+            nome,
+            cargo,
+            imagem,
+            time
+        });
     }
     return (
         <section className='formulario'>
