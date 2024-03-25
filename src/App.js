2811a0d91f54d68ecb7a3a0ffa276197c86ed309
Vinicius Luiz <077viniciu@gmail.com>
2024-03-25 12:04:33 -0300

    iniciando o componente time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
+import Time from './components/Time';
 
 function App() {
 
@@ -17,6 +18,9 @@ function App() {
       <Formulario 
       aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}
       />
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="DataScience"/>
     </div>
   );
 };
