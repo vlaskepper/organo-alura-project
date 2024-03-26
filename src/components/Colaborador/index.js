6e09f29c4f743936cc3ca2657d6122b73b8978b1
Vinicius Luiz <077viniciu@gmail.com>
2024-03-26 13:54:27 -0300

    criando o componente de cards e estilizando

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/vlaskepper.png' alt='Vinícius Luiz'/>
+            </div>
+
+            <div className='rodape'>
+                <h4>Vinícius Luiz</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    );
+} ;
+
+export default Colaborador;
